fbf7f846308ed7334bbd87d0761921dbd6dd33a1
Yanick <rt-2@rt-2.net>
2018-06-26 09:38:29 -0400

    fixed indentation

--- Web-Client/index.php
+++ Web-Client/index.php
@@ -1,49 +1,55 @@
 <?php
-    /*
-        Some PHP code here
-    */
+	/*
+		Some PHP code here
+	*/
 ?><!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
-        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1">
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1">
 
-        <title>Title here</title>
-        <meta name="description" content="Description here" />
+	<title>Title here</title>
+	<meta name="description" content="Description here" />
+
+
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/5.0.0/sanitize.min.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo $config['BASE_URL']; ?>styles/base.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo $config['BASE_URL']; ?>styles/fonts.css" />
+	<link rel="stylesheet/less" type="text/css" href="<?php echo $config['BASE_URL']; ?>styles/styles.less" />
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.2/less.min.js"></script>
+
+</head>
+
+<body class="smooth-font">
+	<div id="content" class="container">
+	<header>
 		
-        
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/5.0.0/sanitize.min.css" />
-        <link rel="stylesheet" type="text/css" href="<?php echo $config['BASE_URL']; ?>styles/base.css" />
-        <link rel="stylesheet" type="text/css" href="<?php echo $config['BASE_URL']; ?>styles/fonts.css" />
-        <link rel="stylesheet/less" type="text/css" href="<?php echo $config['BASE_URL']; ?>styles/styles.less" />
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.2/less.min.js"></script>
+	</header>
+	<nav>
+		
+	</nav>
+	<aside>
+		
+	</aside>
+	<main>
 
-    </head>
+	This website is not using any image;<br />
 
-    <body class="smooth-font">
-        <div id="content" class="container">
-		    <header>
-		    </header>
-		    <nav>
-            </nav>
-		    <aside>
-		    </aside>
-		    <main>
-            
-                This website is not using any image;<br />
-                
-            </main>
-			<footer>
-		        <div class="">
-                    <div class="">&copy;</div>
-                    <div class="">2010-<?php echo date("Y"); ?></div>
-                    <div class="">Yanick<br />Coulombe</div>
-                </div>
-                footer | Footer1 | <a href="#">href 1</a> | <a href="#">Old Website</a>
-			</footer>
-        </div>
-    </body>
+	</main>
+	<footer>
+		
+		<div class="">
+			<div class="">&copy;</div>
+			<div class="">2010-<?php echo date("Y"); ?></div>
+			<div class="">Yanick<br />Coulombe</div>
+		</div>
+		
+		footer | Footer1 | <a href="#">href 1</a> | <a href="#">Old Website</a>
+		
+	</footer>
+	</div>
+</body>
 </html>
