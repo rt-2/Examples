e366e87cd1bc8a2255e3a046f22fc72d41cfa4b4
Yanick <rt-2@rt-2.net>
2018-06-26 08:50:37 -0400

    fixed indentations

--- Web-Client/index.php
+++ Web-Client/index.php
@@ -36,14 +36,14 @@
                 This website is not using any image;<br />
                 
             </main>
-		    <footer>
+			<footer>
 		        <div class="">
                     <div class="">&copy;</div>
                     <div class="">2010-<?php echo date("Y"); ?></div>
                     <div class="">Yanick<br />Coulombe</div>
                 </div>
                 footer | Footer1 | <a href="#">href 1</a> | <a href="#">Old Website</a>
-		    </footer>
+			</footer>
         </div>
     </body>
 </html>
