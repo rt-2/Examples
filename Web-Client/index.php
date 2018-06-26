ea5704ce77bc364560cd5aa7618c60b3d00c491e
Yanick <rt-2@rt-2.net>
2018-06-26 19:00:34 -0400

    added config, baseurl and font awesome

--- Web-Client/index.php
+++ Web-Client/index.php
@@ -1,4 +1,5 @@
 <?php
+    	require_once('./includes/config.inc.php');
 	/*
 		Some PHP code here
 	*/
@@ -13,17 +14,19 @@
 	<title>Title here</title>
 	<meta name="description" content="Description here" />
 
+    	<base href="<?php echo $config['BASE_URL']; ?>">
 
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/5.0.0/sanitize.min.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo $config['BASE_URL']; ?>styles/base.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo $config['BASE_URL']; ?>styles/fonts.css" />
-	<link rel="stylesheet/less" type="text/css" href="<?php echo $config['BASE_URL']; ?>styles/styles.less" />
-	<!-- <link rel="stylesheet" type="text/css" href="<?php echo $config['BASE_URL']; ?>styles/tests.css" /> -->
+	<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/5.0.0/sanitize.min.css" />
+	<!--<script defer src="//use.fontawesome.com/releases/v5.1.0/js/all.js" integrity="sha384-3LK/3kTpDE/Pkp8gTNp2gR/2gOiwQ6QaO7Td0zV76UFJVhqLl4Vl3KL1We6q6wR9" crossorigin="anonymous"></script>-->
+	<link rel="stylesheet" type="text/css" href="styles/base.css" />
+	<link rel="stylesheet" type="text/css" href="styles/fonts.css" />
+	<link rel="stylesheet/less" type="text/css" href="styles/styles.less" />
+	<!-- <link rel="stylesheet" type="text/css" href="styles/tests.css" /> -->
 	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.0.2/less.min.js"></script>
 	
-    	<!--<script src="<?php echo $config['BASE_URL']; ?>scripts/antizoom-0.0.1.js"></script>-->
+    	<!--<script src="scripts/antizoom-0.0.1.js"></script>-->
 
 </head>
 
