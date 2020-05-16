2125b595722e86e2b59b02ff58d2aabd994f8cc8
Yanick <rt-2@rt-2.net>
2020-05-16 05:17:13 -0400

    Update config.inc.php

--- Web-Client/includes/config.inc.php
+++ Web-Client/includes/config.inc.php
@@ -4,8 +4,10 @@
     //Includes
     require_once(__DIR__.'/seo.config.inc.php');
     //Constant(s)
+    define('DOMAIN', 'Example.org');
+    define('PROTOCOL', 'http');
+    define('URL', PROTOCOL.'://'.DOMAIN);
     define('CONFIG', [
-        'DOMAIN' => 'Example.org',
         'NAME' => $SEO_Name,
         'KEYWORDS' => implode(', ', $SEO_Keywords),
         'DESCRIPTION' => $SEO_Description,
