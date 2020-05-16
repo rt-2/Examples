c30aed4369c58300d2b7dc06b13a37d814e1fd17
Yanick <rt-2@rt-2.net>
2020-05-16 04:17:29 -0400

    Create seo.config.inc.php

---
+++ Web-Client/includes/seo.config.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$SEO_Name = 'Apex QC';
+$SEO_Keywords = ['Apex', 'Québec', 'Legends', 'Apex Legends', 'Apex Québec', 'Apex Legends Québec'];
+$SEO_Description = <<<EOT
+Groupe pour la communauté Quebecoise de gamer de Apex Legends. 
+Trouver vous des amis pour jouer ensemble, racontez vos histoires de Légendes, faites des streams, mettez des photos ou des vidéos, trollez, amusez vous surtout.
+EOT;
+
+?>
